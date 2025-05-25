9db8e0bed396acab05b9113f32a3e9393abfeb6c
surbhi <surbhibisarwal79@gmail.com>
2025-05-25 14:08:45 +0530

    added termination

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
-import './App.css'
-import Header from './header'
-import Sidebar from './Sidebar'
-import DashboardMainContent from './DashboardMainContent'
+import './App.css';
+import Header from './header';
+import Sidebar from './Sidebar';
+import DashboardMainContent from './DashboardMainContent';
 
 function App() {
     return (
